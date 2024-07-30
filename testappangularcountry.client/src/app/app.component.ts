e5c1b3f78a3b23fe47308deb66f29099c3ab15aa
IHVH <IHVH@inbox.ru>
2024-07-30 16:07:47 +0300

    Добавьте файлы проекта.

---
+++ testappangularcountry.client/src/app/app.component.ts
@@ -0,0 +1,39 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+interface WeatherForecast {
+  date: string;
+  temperatureC: number;
+  temperatureF: number;
+  summary: string;
+}
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent implements OnInit {
+  public forecasts: WeatherForecast[] = [];
+  public text: string = "";
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    this.getForecasts();
+  }
+
+  getForecasts() {
+    this.http.get<WeatherForecast[]>('/weatherforecast').subscribe(
+      (result) => {
+        this.forecasts = result;
+      },
+      (error) => {
+        console.error(error);
+      }
+    );
+  }
+
+  title = 'testappangularcountry.client';
+}
